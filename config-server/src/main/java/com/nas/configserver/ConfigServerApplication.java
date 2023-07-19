0f0d89853971e8db6a5fb48397993063d220d163
Naschwin Domnic Dias <naschwindias@gmail.com>
2023-07-19 23:06:12 +0530

    added config server module

---
+++ config-server/src/main/java/com/nas/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nas.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
